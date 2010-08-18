daabbde65d64af22aa7d318c06859551db71b83a
Axel Vergult <axel.vergult@gmail.com>
2010-08-18 15:28:32 +0200

    Initializing project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skwize::Application.config.secret_token = '89e71d148a870e072a5ce79a94667e2c597bc05d8e226dd4b99fc89ace8ac5ce1894b8e61292d9dfa9b80542678378af331d1f7f8dd05f2eee2ba56e2fb8a770'
